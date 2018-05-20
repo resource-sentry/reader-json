84eaeddd417190630292e4f0a2ae7fd8bf98ee6b
Nicolas Siver <nicolas@siver.im>
2018-05-20 17:39:04 -0400

    add test coverage for the order

---
+++ test/order.spec.js
@@ -0,0 +1,36 @@
+const Categories = require('@resource-sentry/utils/lib/categories');
+
+const Reader = require('../src/index');
+
+describe('Reader', () => {
+
+    let categories;
+
+    beforeAll(() => {
+        let reader = new Reader({entry: './test/data/order.json'});
+        return reader
+            .scan()
+            .then(() => {
+                categories = reader.getAllCategories();
+            });
+    });
+
+    it('sorts text category', () => {
+        expect(categories[Categories.TEXT]).toEqual([
+            {name: 'textA', value: 'one'},
+            {name: 'textB', value: 'two'},
+            {name: 'textC', value: 'three'},
+            {name: 'textD', value: 'four'}
+        ]);
+    });
+
+    it('sorts value category', () => {
+        expect(categories[Categories.VALUE]).toEqual([
+            {name: 'a', value: 1},
+            {name: 'b', value: 2},
+            {name: 'c', value: 3},
+            {name: 'd', value: 4}
+        ]);
+    });
+
+});
